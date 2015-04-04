bd0cf03656f44ef89ccfe6888cd19e902a11c868
payallmoney <payallmoney@163.com>
2015-04-04 22:58:03 +0800

    1,完成html容器

---
+++ app/src/main/java/com/payallmoney/www/htmlcontainer/MainActivity.java
@@ -0,0 +1,62 @@
+package com.payallmoney.www.htmlcontainer;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        refreshHtml();
+    }
+
+    public  void refreshHtml(){
+        WebView webview =  (WebView) findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient());
+        webview .getSettings().setJavaScriptEnabled(true);
+        webview .getSettings().setDomStorageEnabled(true);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        webview.loadUrl("http://"+prefs.getString("ip","")+"/");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //显示设置窗口
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
